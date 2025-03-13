ab4c2131434f7a1ec4122a6fd6061bc83ae23728
abrhamzewdu2119 <abrhamzewdu2119@gmail.com>
2025-03-13 16:43:56 +0300

    intialize the project54

---
+++ data/index.ts
@@ -0,0 +1,273 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Service", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Notification Service For Bizifyspot",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Besew Online Hybrid Marketplace Mobile App",
+    des: "Discover the features of our innovative marketplace app, offering a seamless hybrid shopping experience.",
+    img: "/besew.png",
+    iconLists: ["/next.svg","/mobile-app.svg",  "/tail.svg", "/ts.svg", "/nest.svg", "/pulsar.svg"],
+    link: "https://play.google.com/store/apps/details?id=com.besewonline",
+  },
+  {
+    id: 2,
+    title: "Bizify Spot",
+    des: "Enhance your video conferencing with Yoom. Effortlessly connect with colleagues and friends.",
+    img: "/bizfy.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/express.svg", "/socket-io-white.svg", "/pulsar.svg"],
+    link: "https://qa.bizfyspot.com/",
+  },
+  {
+    id: 3,
+    title: "Ethio Heran Tutor",
+    des: "A personal tutoring website catering to students from primary school to university.",
+    img: "/ethio-heran.png",
+    iconLists: ["/re.svg", "/tail.svg", "/express.svg", "/nodejs.svg"],
+    link: "https://ethioherantutoring.com/",
+  },
+  {
+    id: 4,
+    title: "M-POS Mobile App",
+    des: "Experience the recreated Apple iPhone 15 Pro website, featuring GSAP animations and Three.js 3D effects.",
+    img: "/p4.svg",
+    iconLists: ["/re.svg","mobile-app.svg", "/tail.svg", "/socket-io-white.svg", "nodejs.svg"],
+    link: "https://play.google.com/store/apps/details?id=com.addismpos",
+  },
+  {
+    id: 5,
+    title: "Bluenile resort",
+    des: "A platform for online tutoring, connecting tutors with parents and students.",
+    img: "/bluenile.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/express.svg", "/nodejs.svg"],
+    link: "https://bluenileresorthotels-avanti.com/",
+  },
+  {
+    id: 6,
+    title: "Warehouse Management System",
+    des: "Efficiently manage your inventory with our comprehensive warehouse management system.",
+    img: "/warehouse.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg","/nodejs.svg","/socket-io-white.svg"],
+    link: "",
+  },
+  {
+    id: 7,
+    title: "Restaurant Website Landing Page",
+    des: "A beautiful landing page for a restaurant website, available in multiple languages.",
+    img: "/restaurant.png",
+    iconLists: ["/re.svg", "/tail.svg"],
+    link: "https://restaurantalpha.onrender.com/",
+  },
+  {
+    id: 8,
+    title: "Computer Monitoring System Desktop App",
+    des: "Computer hardwares performance and Space Usage Monitoring System",
+    img: "/system-monitor.png",
+    iconLists: ["/python.svg", "/qt.svg"],
+    link: "",
+  },
+  {
+    id: 9,
+    title: "Computer Vision Security System",
+    des: "A Security System mainly using Computer Cameras to Send Peoples images around pc with email to prevent stealing",
+    img: "/opencv-test.jpg",
+    iconLists: ["/python.svg", "/qt.svg", "/opencv.svg", "/twilio.svg"],
+    link: "",
+  },
+];
+
+
+export const testimonials = [
+  {
+    quote:
+      "Crafting visually stunning and highly responsive websites tailored to your brand's unique identity. Specializing in modern design principles and user experience.",
+    name: "Web Design",
+    title: "Creative & Responsive Design",
+  },
+  {
+    quote:
+      "Developing robust and scalable web applications using the latest technologies. Ensuring seamless performance and security to meet your business needs.",
+    name: "Web Development",
+    title: "Full-Stack Development",
+  },
+  {
+    quote:
+      "Providing expert SEO services to improve your website's visibility and ranking on search engines. Implementing effective strategies to drive organic traffic.",
+    name: "SEO Optimization",
+    title: "Search Engine Optimization",
+  },
+  {
+    quote:
+      "Creating engaging and informative content to attract and retain your target audience. Specializing in blog posts, articles, and website copy.",
+    name: "Content Creation",
+    title: "Engaging & Informative Content",
+  },
+  {
+    quote:
+      "Designing intuitive mobile applications for iOS and Android platforms. Focusing on user-friendly interfaces and smooth user experiences.",
+    name: "Mobile App Development",
+    title: "iOS & Android Apps",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "NEXT",
+    img: "",
+    nameImg: "/NEXT.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+  
+    nameImg: "/pulsar.svg",
+  },
+  {
+    id: 3,
+    name: "nest",
+    img: "/nest.svg",
+    nameImg: "",
+  },
+  {
+    id: 4,
+    name: "nest",
+    img: "/socket-io-white.svg",
+    nameImg: "",
+  },
+  {
+    id: 5,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 6,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+    {
+    id: 7,
+    name: "qt",
+    img: "/qt.svg",
+    nameImg: "",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Lead in the development of a web-based Clearance system using React.js and Express.js for Addis Ababa Science and Technology University.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - Addis Pay",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Web and App Dev Project",
+    desc: "Led the dev of a mobile and web app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "FullStack Web and Mobile App Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies and oversaw backend development and integration to ensure seamless full-stack solutions",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link:"http://github.com/1-aminadab"
+  },
+  {
+    id: 2,
+    img: "/telegram-white.svg",
+    username:"@ready2develop"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link:"www.linkedin.com/in/amanueltadesse-870416232"
+  },
+];
